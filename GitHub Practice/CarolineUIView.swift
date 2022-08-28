846e9de0e4058d9d6503faa9802828c1045cfd08
Caroline Willis <carolinewillis211@gmail.com>
2022-08-28 14:39:57 -0700

    create second view

---
+++ GitHub Practice/CarolineUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  GitHub Practice
+//
+//  Created by Caroline Willis on 8/28/22.
+//
+
+import SwiftUI
+
+struct CarolineUIView: View {
+    var body: some View {
+        Text("Caroline")
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        CarolineUIView()
+    }
+}
